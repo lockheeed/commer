e1b5886dad6a6f41716a016c3a2318edfca6e792
root <root@localhost.localdomain>
2019-06-09 22:33:27 +0300

    Первый релиз commer

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 # !/bin/bash
 
 apt install python3
+apt install python
 pip3 install vk_api
 pip3 install requests
+pip install vk_api
+pip install requests
 echo -e '\n\e[32;1m[ + ] Установка завершена! Обязательно прочтите README.md\e[0m'
 echo '[ > ] Для запуска commer используй: python3 commer.py'
